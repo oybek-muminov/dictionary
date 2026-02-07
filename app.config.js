a995f339a13218657e03bea648ea00624a911c9b
oybek-muminov <oybekmominov227@gmail.com>
2026-02-07 12:28:29 +0500

    chore: set frontend VAPID public key

--- app.config.js
+++ app.config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://xekuioexpgvcnosxarox.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhla3Vpb2V4cGd2Y25vc3hhcm94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0MzY0NjYsImV4cCI6MjA4NjAxMjQ2Nn0.iamRwx5reW3umZyW5fbEiAe-2RsKTAmo1Qjc_dsxJbY",
-  VAPID_PUBLIC_KEY: ""
+  VAPID_PUBLIC_KEY: "BBPyjwFvjw8i75zxyIcPfwc4PF_n_fokaaX4LzwAW0jXhbU4ZWs7YguBd8ynvwLzz3mdD5Vfft-TVxT-UQ5TdPs"
 };
